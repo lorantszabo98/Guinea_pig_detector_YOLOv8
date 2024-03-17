8893213a96c8f1ca30e03c38f39dc032382aee03
lorantszabo98 <64622519+lorantszabo98@users.noreply.github.com>
2024-03-17 23:19:15 +0100

    Added data

---
+++ data/README.dataset.txt
@@ -0,0 +1,6 @@
+# Guinea Pig Detection > 2024-03-14 8:40pm
+https://universe.roboflow.com/projects-josub/guinea-pig-detection-grlwn
+
+Provided by a Roboflow user
+License: CC BY 4.0
+
--- inference.py
+++ inference.py
@@ -1,5 +1,7 @@
 from ultralytics import YOLO
 
-model = YOLO('best.pt')
+model = YOLO('best_n_new.pt')
 
-results = model(source='./test images/ervinke.jpeg', conf=0.5, save=True)
+# results = model(source='./test images/sarika_lujzika.jpg', conf=0.2, save=True)
+
+results = model.track(source="https://youtu.be/eW6hheq8qfg?si=wXpYHm2e1pD6Rfnx", show=True)
